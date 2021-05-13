67fb7070845d415bb84206cc23b6aa6ab0b553d0
Alexey R <alex666alex@mail.ru>
2021-05-14 00:01:48 +0300

    testing husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-import S from './app.modules.scss';
+import S from './app.module.scss';
 
 const App = () => {
-    return (
-        <div className={S.header}>
-            TemplateReactApp
-        </div>
-    )
+    return <div className={S.header}>TemplateReactApp</div>;
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,60 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 const NODE_ENV = process.env.NODE_ENV;
-console.log('_________________________');
-console.log('ENV', NODE_ENV);
-console.log('_________________________');
+console.log("_________________________");
+console.log("ENV", NODE_ENV);
+console.log("_________________________");
 module.exports = {
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-    },
-    mode: NODE_ENV ? NODE_ENV : 'development',
-    entry: path.resolve(__dirname, 'src/index.ts'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.[tj]sx?$/,
-                exclude: /node_modules/,
-                use: ['ts-loader'],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  mode: NODE_ENV ? NODE_ENV : "development",
+  entry: path.resolve(__dirname, "src/index.ts"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.module[s]?\.scss$/,
+        use: [
+          "style-loader",
+          "css-modules-typescript-loader?modules",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                mode: "local",
+                localIdentName: "[name]__[local]__[hash:base64:5]",
+                auto: /\.module[s]?\.\w+/,
+              },
             },
-            // {
-            //     test: /\.css$/,
-            //     use: ['style-loader', 'css-loader']
-            // },
-            {
-                test: /\.(s*)css$/,
-                use: [
-                    'style-loader',
-                    'css-modules-typescript-loader?modules',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                mode: 'local',
-                                localIdentName: '[name]__[local]__[hash:base64:5]',
-                                auto: /\.module[s]?\.\w+/,
-                            },
-                        }
-                    },
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'public/index.html')
-        })
+          },
+          "sass-loader",
+        ],
+      },
     ],
-    devServer: {
-        port: 3000,
-        open: true,
-        hot: true,
-    },
-    devtool: 'source-map'
-}
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  devtool: "source-map",
+};
