8a5e27c4462b6db3bbb6e6d0a520fa738dd61903
Alexey R <alex666alex@mail.ru>
2021-05-23 20:33:00 +0300

    little fix for endpoint type / keyof typeof config.client.endpoint

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,8 @@
 import Url from 'url';
 import getUrlWithParams from './getUrlWithParams';
+import config from '../config';
 
-async function request(endpoint: string, limit: number = 15) {
+async function request(endpoint: keyof typeof config.client.endpoint, limit: number = 15) {
     const uri = Url.format({ ...getUrlWithParams(endpoint), query: { limit } });
     return fetch(uri).then((res) => res.json());
 }
