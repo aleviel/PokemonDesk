7023190aade529e9c9056455a0d6032de747682f
Alexey R <alex666alex@mail.ru>
2021-05-31 23:22:47 +0300

    test for button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,11 +5,11 @@ import Style from './button.module.scss';
 
 interface IButtonProps {
     onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    size?: 'small' | 'wide';
+    size?: 'small' | 'wide' | 'default';
     color?: 'green' | 'yellow';
 }
 
-const Button: React.FC<IButtonProps> = ({ children, onClick, color, size }) => {
+const Button: React.FC<IButtonProps> = ({ children, onClick, color = 'green', size = 'default' }) => {
     return (
         <button
             className={CN(Style.root, Style[size as keyof typeof Style], Style[color as keyof typeof Style])}
